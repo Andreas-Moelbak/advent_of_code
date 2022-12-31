1c75e14917dbd2f25d5bfaf365b5c14a289f6249
Andreas Mølbak <anm@dubex.dk>
2022-12-31 16:11:16 +0100

    solution to day6 and get-puzzle script added

--- day6.py
+++ day6.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+
+
 with open("day6.input", "r") as file:
-    data = file.read().strip().split("\n")
+    data = file.read().strip()
 
+# Part One
+for idx, char in enumerate(data):
+    if idx > 3:
+        liste = data[idx-4:idx]
+        if len(Counter(liste)) == 4:
+            print(idx)
+            break
 
-for i in data:
-    print(i)
+# Part Two
+for idx, char in enumerate(data):
+    if idx > 13:
+        liste = data[idx-14:idx]
+        if len(Counter(liste)) == 14:
+            print(idx)
+            break
---
+++ get-puzzle.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+YEAR=2022
+DAY=$1
+SESSION=$(cat session_cookie.txt)
+OUTFILE="day$DAY.input"
+SOLVEFILE="day$DAY.py"
+
+curl --cookie "session=$SESSION" https://adventofcode.com/$YEAR/day/$DAY/input > $OUTFILE
+cp ./template.py $SOLVEFILE && sed -i '' "s/day.input/day$1.input/g" $SOLVEFILE && chmod +x $SOLVEFILE
