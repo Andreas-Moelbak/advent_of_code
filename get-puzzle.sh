8344d7d6607792a331d31f31d4162a2543b9bc14
Andreas Mølbak <andreas.moelbak@protonmail.com>
2025-02-14 20:58:06 +0100

    updated puzzle download script

--- get-puzzle.sh
+++ get-puzzle.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-YEAR=$2
+
 DAY=$1
-SESSION=$(cat session_cookie.txt)
-OUTFILE="day$DAY.input"
-SOLVEFILE="day$DAY.py"
+YEAR=$2
+SESSION=53616c7465645f5f7c297faef16e669072ffc421cfecceb33b4628e98581e070ad6245976523c0eefb3f843461c8f3eb1cc66fdb426c31880c49b34f216f9614
+OUTFILE="$YEAR/day$DAY.input"
+SOLVEFILE="$YEAR/day$DAY.py"
 
 curl --cookie "session=$SESSION" https://adventofcode.com/$YEAR/day/$DAY/input > $OUTFILE
-cp ./template.py $SOLVEFILE && sed -i '' "s/day.input/day$1.input/g" $SOLVEFILE && chmod +x $SOLVEFILE
+cp ./template.py $SOLVEFILE && sed -i "s/day.input/day$DAY.input/g" $SOLVEFILE && chmod +x $SOLVEFILE
